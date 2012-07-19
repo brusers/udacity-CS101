c449bde40b282f7b7ac0db2c25671979a2258ffe
brusers <marcel@hacked2bits.com>
2012-07-20 00:06:36 +0300

    Updated code

--- webCrawler.py
+++ webCrawler.py
@@ -50,13 +50,43 @@ def crawl_web(seed):
             crawled.append(page)
     return index, graph
 
-def record_user_click(index,keyword,url):
-    urls = lookup(index, keyword)
-    if urls:
-        for entry in urls:
-            if entry[0] == url:
-                entry[1]+=1
+#~ def record_user_click(index,keyword,url):		#???
+    #~ urls = lookup(index, keyword)
+    #~ if urls:
+        #~ for entry in urls:
+            #~ if entry[0] == url:
+                #~ entry[1]+=1
 
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 10
+    
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+    
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph:
+				if page in graph[node]:
+					newrank += d*(ranks[node]/len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 def add_to_index(index, keyword, url):
     if keyword in index:
 		index[keyword].append(url)
@@ -68,12 +98,16 @@ def lookup(index, keyword):
     if keyword in index:
         return index[keyword]
     return None
-    
 
-def add_page_to_index(index,url,content):
-    myList = split_string(content," !,.)
-    for i in range(len(myList)):
-        add_to_index(index,myList[i],url)
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+
+#~ def add_page_to_index(index,url,content):     # Mine
+    #~ myList = split_string(content," !,.)
+    #~ for i in range(len(myList)):
+        #~ add_to_index(index,myList[i],url)
 		
 def split_string(source,splitlist):
     output = []
@@ -129,3 +163,88 @@ def make_hashtable(nbuckets):
     for unused in range(0,nbuckets):
         table.append([])
     return table
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+def lucky_search(index, ranks, keyword):
+	pages = lookup(index, keyword)
+	if not pages:
+		return None
+	best_page = pages[0]
+	for candidate in pages:
+		if ranks[candidate]>ranks[best_page]:
+			best_page = candidate
+	return best_page
+
+
+
+
+
+
+
+
+
+def ordered_search(index, ranks, keyword):
+	pages = lookup(index, keyword)
+	pageList = []
+	if not pages:
+		return None
+	for candidate in pages:
+		pageList.append(candidate)
+	mySort(pageList, ranks)
+	return pageList
+
+def mySort(theList, ranking):
+    for i in range(len(theList)-1):
+        if (ranking[theList[i+1]]) > ranking[theList[i]]:
+            theList[i],theList[i+1] = theList[i+1],theList[i]
+            i=0
+    return theList
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+def ancestors(genealogy, person):
+	if person in genealogy:
+		parents = genealogy[person]
+		result = parents
+		for parent in parents:
+			result = result + ancestors(genealogy, parent)
+		return result
+	
+    
+    
+    
+    
+    
+    
+    
+    
